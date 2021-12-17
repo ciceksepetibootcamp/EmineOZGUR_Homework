9d17c09b4323421d8b30cc1d7114c9a01407b567
Emine ÖZGÜR <94612715+ozguremine01@users.noreply.github.com>
2021-12-17 20:03:34 +0300

    Add files via upload

---
+++ Week 4/AliExpressTests/AliExpressTests/Locators/locators.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AliExpressTest.Locators
+{
+    class locators
+    {
+        //Elementlerin xpath ve idleri
+
+        public String txt_search_product_name_class_name = "//*[@class=\"normalsize blob\"]";
+        public String btn_close_xpath = "//IMG[@class=\"btn-close\"]";
+        public String btn_pop_up_dont_allow_xpath = "//*[@class=\"Sk1_X _1-SOk\"]";
+        public String txt_searchbar_id = "search-key";
+        public String btn_search_button_xpath = "//*[@class=\"search-button\"]";
+        public String lst_search_category_id = "search-dropdown-box";
+        public String btn_select_computer_and_office_xpath = "//option[@value=\"7\"]";
+        public String btn_cart_xpath = "//*[@class=\"right-cart-icon\"]";
+        public String btn_speed_search_xpath = "//a[3]";
+        public String homepage_content_id = "root";
+        public String btn_user_profile_xpath = "//span[@class=\"user-account-port\"]";
+        public String btn_sign_in_xpath = "//a[@class=\"sign-btn\"]";
+        public String txt_email_id = "fm-login-id";
+        public String txt_pass_id = "fm-login-password";
+        public String btn_pop_up_sign_in_xpath = "//*[@class=\"fm-button\"]";
+        public String btn_forgot_xpath = "//*[@class=\"fm-forget\"]";
+        public String btn_pop_up_close_xpath = "//*[@class=\"fm-dialog-close\"]";
+        public String btn_change_language_id = "switcher-info";
+        public String btn_selected_language_xpath = "//*[@class=\"address-select-trigger address-select-trigger-active\"]";
+        public String txt_search_country_xpath = "(//*[@placeholder=\"Ara\"])[1]";
+        public String txt_language_xpath = "(//*[@class=\"select-item\"])[1]";
+        public String txt_language_txt_xpath = "(//INPUT[@placeholder=\"Ara\"])[1]";
+        public String txt_language_english_xpath = "//*[@data-locale=\"en_US\"]";
+        public String btn_first_product_xpath = "(//*[@class=\"oOZ6t product-img\"])[1]";
+        public String txt_min_price_xpath = "//*[@placeholder=\"min\"]";
+        public String txt_max_price_xpath = "//*[@placeholder=\"max\"]";
+        public String btn_narrow_go_xpath = "//*[@class=\"ui-button narrow-go\"]";
+        public String lbl_like_other_products_xpath = "//*[@id=\"root\"]/div[5]/div/h2/div[2]";
+        public String img_first_like_other_product_xpath = "(//*[@class=\"oOZ6t product-img\"])[1]";
+        public String btn_add_to_cart_xpath = "//*[@class=\"next-btn next-large next-btn-primary addcart\"]";
+        public String btn_add_to_favorities_xpath = "//*[@class=\"next-icon next-icon-favourite next-large\"]";
+        public String filter_xpath = "//*[@ae_object_value=\"price(lowest)\"]";
+        public String change_price_filter_xpath = "//*[@data-spm-anchor-id=\"a2g0o.productlist.0.i23.755540aby4tHrC\"]";
+        public String first_product_price_xpath = "(//*[@class=\"_13_ga _37W_B\"])[1]";
+    }
+}
